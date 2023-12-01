35d1ddca40383a9cdab7901d7c9c62c0fbe5cf7b
Jonas Diego Sedeño Perez <jonasperez768@gmail.com>
2023-11-30 19:45:39 -0600

    Método de registro

--- app/src/main/java/com/example/iot_plantmate/RegisterActivity.java
+++ app/src/main/java/com/example/iot_plantmate/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.iot_plantmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +11,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    // Variables para almacenar UID y correo electrónico
+    private String uid;
+    private String email;
 
     EditText edUsername, edEmail, edPassword, edConfirm;
     Button btn;
@@ -20,6 +34,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         edUsername = findViewById(R.id.editTextRegUsername);
         edPassword = findViewById(R.id.editTextRegPassword);
@@ -46,16 +62,39 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Llena todos los campos para continuar", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    // Validar que las contraseñas coincidan
                     if (password.compareTo(confirm) == 0) {
-                        if (isValid(password)){
-                            Toast.makeText(getApplicationContext(), "Registro insertado", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                        }
-                        else {
-                            Toast.makeText(getApplicationContext(), "La contraseña debe contener al menos 8 caracteres, con letra, numero y simbolo especial....", Toast.LENGTH_SHORT).show();
+                        // Validar que la contraseña cumpla con los requisitos
+                        if (isValid(password)) {
+                            // Crear usuario en Firebase Authentication
+                            mAuth.createUserWithEmailAndPassword(email, password)
+                                    .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                // Registro exitoso
+                                                FirebaseUser user = mAuth.getCurrentUser();
+                                                uid = user.getUid(); // Almacenar el UID en la variable
+
+                                                // Guardar el nombre en la base de datos en tiempo real
+                                                mDatabase.child("users").child(uid).child("nombre").setValue(username);
+                                                mDatabase.child("users").child(uid).child("email").setValue(email);
+                                                mDatabase.child("users").child(uid).child("uid").setValue(uid);
+                                                Toast.makeText(getApplicationContext(), "Registro insertado", Toast.LENGTH_SHORT).show();
+
+                                                // Redirigir a HomeActivity
+                                                startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                                            } else {
+                                                // Si falla el registro, mostrar un mensaje de error
+                                                Toast.makeText(getApplicationContext(), "Error al registrar el usuario", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(getApplicationContext(), "La contraseña debe contener al menos 8 caracteres, con letra, número y símbolo especial.", Toast.LENGTH_SHORT).show();
                         }
                     } else {
-                        Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden....", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
